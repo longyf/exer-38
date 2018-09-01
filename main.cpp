cdc35d52989f70ca42288aeb98f4689fc2fad9a2
longyf <longyf@pku.edu.cn>
2018-09-01 20:58:25 +0800

    Add set and vector
    Add set and vector to avoid repeated prints.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <string>
 #include <iostream>
+#include <vector>
+#include <set>
 using namespace std;
 
 void print(string &s1) {
@@ -9,12 +11,12 @@ void print(string &s1) {
 	cout<<endl;
 }
 
-void Permutation(string &s1, string::size_type index) {
+void Permutation(string &s1, string::size_type index, set<string> &set1) {
 	auto i = index;
 	while (i != s1.size()) {
 		swap(s1[index], s1[i]);
-		if (index == s1.size() - 1) print(s1);//index处理到最后一位了才需要输出。
-		Permutation(s1, index + 1);
+		if (index == s1.size() - 1) set1.insert(s1);
+		Permutation(s1, index + 1, set1);
 		swap(s1[index], s1[i]);
 		i++;
 	}
@@ -22,6 +24,15 @@ void Permutation(string &s1, string::size_type index) {
 
 int main() {
 	string s1 = "abc";
-	Permutation(s1, 0);
+	vector<string> v1;
+	set<string> set1;
+	Permutation(s1, 0, set1);
+	for (auto iter = set1.begin(); iter != set1.end(); ++iter) {
+		v1.push_back(*iter);
+	}
+
+	for (auto iter = v1.begin(); iter != v1.end(); ++iter) {
+		print(*iter);
+	}
 	return 0;
 }
